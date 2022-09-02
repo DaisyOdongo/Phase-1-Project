6b1b99d6b2dce2b63311e7a1593796a9487eae51
+ <+>
2022-09-02 22:47:38 +0300

    added click event listen to the header

--- index.js
+++ index.js
@@ -12,3 +12,24 @@ fetch('https://groceries2.p.rapidapi.com/api/v1/groceries', options)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
+
+    let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+let header = document.querySelector('.header-2');
+
+menu.addEventListener('click', () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+});
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+
+    if(window.scrollY > 150){
+        header.classList.add('active');
+    }
+    else{
+        header.classList.remove('active');
+    }
+}
